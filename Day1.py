476c188ac75c8f514041e70a39be6a25e4d61324
Rylande <frankmennink@gmail.com>
2021-12-01 09:53:29 +0100

    Day 1 initial solution

--- Day1.py
+++ Day1.py
@@ -1,1 +1,18 @@
-inputString = open("Input/Day 1.txt", "r").read().splitlines()
+inputString = [int(x) for x in open("Input/Day 1.txt", "r").read().splitlines()]
+
+def partOne():
+    count = 0
+    for index in range(1,len(inputString)):
+        if inputString[index]-inputString[index-1] > 0: 
+            count += 1
+    print(count)
+
+def partTwo():
+    count = 0
+    for index in range(1,len(inputString)-2):
+        if (inputString[index]+inputString[index+1]+inputString[index+2])-(inputString[index-1]+inputString[index]+inputString[index+1]) > 0: 
+            count += 1
+    print(count)
+
+partOne()
+partTwo()
