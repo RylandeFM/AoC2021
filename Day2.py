b383ed9c09497ca63dc0ea405b1c064a9d3bf4ca
Rylande <frankmennink@gmail.com>
2021-12-02 09:58:25 +0100

    Initial solution

--- Day2.py
+++ Day2.py
@@ -1,1 +1,33 @@
 inputString = open("Input/Day 2.txt", "r").read().splitlines()
+
+def partOne():
+    [hor, dep] = [0, 0]
+    for line in inputString:
+        [com, amt] = line.split(" ")
+        if (com == "forward"):
+            hor += int(amt)
+        elif (com == "down"):
+            dep += int(amt)
+        elif (com == "up"):
+            dep -= int(amt)
+        else:
+            print("Error")
+    print(hor * dep)
+
+def partTwo():
+    [hor, dep, aim] = [0, 0, 0]
+    for line in inputString:
+        [com, amt] = line.split(" ")
+        if (com == "forward"):
+            hor += int(amt)
+            dep += int(amt) * aim
+        elif (com == "down"):
+            aim += int(amt)
+        elif (com == "up"):
+            aim -= int(amt)
+        else:
+            print("Error")
+    print(hor * dep)
+
+partOne()
+partTwo()
