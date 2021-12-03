41c87ee280f261ddbe61e9b6511994ea85d0152a
Rylande <frankmennink@gmail.com>
2021-12-03 14:25:24 +0100

    Refactor

--- Day3.py
+++ Day3.py
@@ -1,24 +1,22 @@
-inputString = open("Input/Day 3.txt", "r").read().splitlines()
+inputList = open("Input/Day 3.txt", "r").read().splitlines()
 
 def solveBoth():
-    diagList, g, e = [], "", ""
-    for line in inputString:
-        diagList.append(line)
+    g, e = "", ""
 
-    for i in range(len(diagList[0])):
-        s = sum([int(x[i]) for x in diagList]) 
-        g += "0" if s < len(diagList)/2 else "1"
-        e += "1" if s < len(diagList)/2 else "0"
+    for i in range(len(inputList[0])):
+        s = sum([int(x[i]) for x in inputList]) 
+        g += "0" if s < len(inputList)/2 else "1"
+        e += "1" if s < len(inputList)/2 else "0"
 
     print(int(g,2)*int(e,2))
-    print(int(findRating(diagList[:],"0","1"),2)*int(findRating(diagList[:],"1","0"),2))
+    print(int(findRating(inputList[:],"0","1"),2)*int(findRating(inputList[:],"1","0"),2))
 
-def findRating(diagList, positive, negative):
+def findRating(input, positive, negative):
     currentPos = 0
-    while len(diagList) > 1:
-        bit = positive if sum([int(x[currentPos]) for x in diagList]) < len(diagList)/2 else negative
-        diagList = [x for x in diagList if x[currentPos]==bit]
+    while len(input) > 1:
+        bit = positive if sum([int(x[currentPos]) for x in input]) < len(input)/2 else negative
+        input = [x for x in input if x[currentPos]==bit]
         currentPos += 1
-    return diagList[0]
+    return input[0]
 
 solveBoth()
