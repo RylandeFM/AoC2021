fee64c9e7902a0cadb19c3282ccb2f39b188dd30
Rylande <frankmennink@gmail.com>
2021-12-07 11:09:42 +0100

    More optimized solution

--- Day7.py
+++ Day7.py
@@ -1,23 +1,10 @@
 inputString = [int(i) for i in open("Input/Day 7.txt", "r").read().splitlines()[0].split(",")]
-#inputString = [16,1,2,0,4,2,7,1,2,14]
 
 def partOne():
-    minFuel = 9999999999999999
-    for i in range(min(inputString), max(inputString)+1):
-        sumOfFuel = sum([abs(i - x) for x in inputString])
-        if sumOfFuel < minFuel:
-            minFuel = sumOfFuel
-            minI = i
-    print(minI, minFuel)
+    print(min(sum(abs(i - x) for x in inputString) for i in range(min(inputString), max(inputString) + 1)))
 
 def partTwo():
-    minFuel = 9999999999999999
-    for i in range(min(inputString), max(inputString)+1):
-        sumOfFuel = sum([sum(range(abs(i - x)+1)) for x in inputString])
-        if sumOfFuel < minFuel:
-            minFuel = sumOfFuel
-            minI = i
-    print(minI, minFuel)
+    print(min(sum(int(abs(i - x)*((abs(i - x)+1)/2)) for x in inputString) for i in range(min(inputString), max(inputString) + 1)))
 
 partOne()
 partTwo()
