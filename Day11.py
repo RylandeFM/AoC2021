8fd8004cd1782f86be6a758626bcb828391cac5d
Rylande <frankmennink@gmail.com>
2021-12-12 14:06:15 +0100

    Methodized board update

--- Day11.py
+++ Day11.py
@@ -16,14 +16,12 @@ def solveBoth():
         while len(flashes) > 0:
             for flash in flashes:
                 hasFlashed.append(flash)
-                for x in range(max(0, flash[0]-1),min(len(octopusEnergy), flash[0]+2)):
-                    for y in range(max(0, flash[1]-1),min(len(octopusEnergy[0]), flash[1]+2)):
-                        octopusEnergy[x][y] += 1
+                updateFlashes(octopusEnergy, flash)
             flashes = getFlashing(octopusEnergy, hasFlashed)
 
         #set energy to 0 for flashed
-        for coord in hasFlashed:
-            octopusEnergy[coord[0]][coord[1]] = 0
+        for (x, y) in hasFlashed:
+            octopusEnergy[x][y] = 0
 
         totalFlashes += len(hasFlashed)
         if currentStep == 100: print(totalFlashes)
@@ -31,5 +29,10 @@ def solveBoth():
 
 def getFlashing(board, hits):
     return [(i,j) for i in range(len(board)) for j in range(len(board[0])) if board[i][j]>9 and (i,j) not in hits]
+
+def updateFlashes(board, flash):
+    for x in range(max(0, flash[0]-1), min(len(board), flash[0]+2)):
+        for y in range(max(0, flash[1]-1), min(len(board[0]), flash[1]+2)):
+            board[x][y] += 1
 
 solveBoth()
