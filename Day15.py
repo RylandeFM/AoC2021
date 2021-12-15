34a6e627787f1ac9d98542e976df5f6a4ad34643
Rylande <frankmennink@gmail.com>
2021-12-15 13:27:57 +0100

    Initial solution

--- Day15.py
+++ Day15.py
@@ -1,1 +1,39 @@
+from collections import deque
+
 inputString = open("Input/Day 15.txt", "r").read().splitlines()
+grid = [[int(x) for x in line] for line in inputString]
+
+def findRoute(start, end):
+    visited = {}
+    queue = deque([start])
+
+    while len(queue) > 0:
+        x, y = queue.pop()
+        risk = visited.get((x,y), 0)
+        for a,b in getNeighbours((x,y), end):
+            newRisk = risk + grid[a][b]
+            if (a,b) not in visited.keys() or visited[(a, b)] > newRisk:
+                visited[(a, b)] = newRisk
+                queue.appendleft((a,b))
+    print(visited[end])
+
+def getNeighbours(point, end):
+    x, y = point
+    for a, b in (0, 1), (1, 0), (0, -1), (-1, 0):
+        if 0 <= (x + a) <= end[0] and 0 <= (y + b) <= end[1]:
+            yield (x + a, y + b)
+
+def increaseGrid(oldGrid):
+    newGrid = []
+    for i in range(5):
+        for line in oldGrid:
+            newLine = []
+            for j in range(5):
+                newLine += [((i + j + n - 1) % 9) + 1 for n in line]
+            newGrid.append(newLine)
+    return newGrid
+
+
+findRoute((0,0), (len(grid)-1, len(grid[0])-1))
+grid = increaseGrid(grid)
+findRoute((0,0), (len(grid)-1, len(grid[0])-1))
